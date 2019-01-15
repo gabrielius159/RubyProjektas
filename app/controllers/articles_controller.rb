15de7d8066d3dd6b6b704f79ed6e4955bab3fafa
gabrielius159 <gabrielius.salichovas@gmail.com>
2019-01-15 14:34:55 +0200

    Pridėjau linkus, show veiksmą

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,20 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     # sukuriamas kintamasis, kad galėtume naudoti formą new.html.erb
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -16,9 +29,17 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def update
     @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
+
+
 
   private
   def article_params
