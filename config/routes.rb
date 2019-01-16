216cebff9d364ea6e22922007400dbbed82f13f7
gabrielius159 <gabrielius.salichovas@gmail.com>
2019-01-16 15:49:46 +0200

    Signup users iš UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
 
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resource :users, except: [:new]
+
   # POST, DELETE, SELECT, PUT, UPDATE ir etc. articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
